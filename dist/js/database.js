9a44c777939e34298c8299d85bd8cb3d5b94984b
Moss Doerksen <lomacar@gmail.com>
2014-10-04 19:27:41 -0700

    Lots to do with tags, tag implication (ontology), and lexicon processing

--- dist/js/database.js
+++ dist/js/database.js
@@ -59,8 +59,17 @@ function processLexicon(data, type){
 
         delete a.rowNumber //something that Tabletop seems to add in
         prune(a)
+        for(var key in a) {
+            if(a[key] === " ") a[key] = ""
+        }
 
-        setProto(a,type,x)
+        lookup[type][a.name] = x
+
+    }
+
+    //Prototypes
+    for (x in database[type]) {
+        setProto(database[type][x],type,x)
     }
 
     //Senses
@@ -68,71 +77,113 @@ function processLexicon(data, type){
         createSenses(database[type][x],type)
     }
 
-    if(type!=='noun') return
+    if(type=='noun') {
 
-    //temporarily turn tags into a sub-object of each word
-    for (var z in database[type]) {
-        if ("tags".in(database[type][z])){
-            var t = database[type][z].tagsObject = {}
-            toObject( database[type][z].tags.split(',').forEach(function(y){t[y]=y}) )
-        }
-    }
 
-    //then do complicated merging of tag objects
-    for (var z in database[type]) {
-        var a = database[type][z]
-        var proto = pickOne(database[type], {name: a.proto})
+        ALL_TAGS = []
+        //add implied tags to words
+        $.get("../csv/ontology.tgf", function(data){
+            var stuff = data.split('#')
+            var nodes = stuff[0].split('\r\n').slice(0,-1).map(function(x){return x.replace(/\d+ (.*)/,'$1')})
+            var paths = stuff[1].split('\r\n').slice(1,-1).map(function(x){return x.split(' ')})
+            ontology = {}
 
-        //merge tags from proto, unless there is a dash, which means don't merge
-        if (a.tagsObject && proto && proto.tagsObject && !a.tags.findChar('-')) {
-            //////////////////////////////////////////////////////////
-            //bob = {b:1}; sally = {s:2}; jack = {j:3}; sally = Object.setPrototypeOf(sally, bob); jack = Object.setPrototypeOf(jack, bob)
-            //narf = Object.setPrototypeOf(narf,jack); narf = _.extend(narf,sally);
-            //////////////////////////////////////////////////////////
-
-            //basically you gotta do the same thing for tags that you do for the word as a whole
-            if (a.tagsObject !== proto.tagsObject) {
-                //add tags from prototype
-                a.tagsObject = Object.setPrototypeOf(a.tagsObject, proto.tagsObject )
+            for(n in nodes){
+                ontology[nodes[n]] = []
             }
-            if ( a.parallelSense ) {
-                //add any extra tags from parallel sense
-                a.tagsObject = _.extend( a.tagsObject,  _.clone(a.parallelSense.tagsObject) )
-                                                        //clone grabs just the ownProperties
+            for(var p in paths){
+                ontology[nodes[paths[p][1]-1]].push(nodes[paths[p][0]-1])
+            }
+
+            for (var w in database[type]) {
+                if(database[type][w].tags) {
+                    var tag_array = database[type][w].tags.split(',')
+                    database[type][w].tags = addImpliedTags(tag_array).join(',')
+                }
+            }
+        })
+
+        //temporarily turn tags into a sub-object of each word
+        for (var z in database[type]) {
+            if ("tags".in(database[type][z])){
+                var t = database[type][z].tagsObject = {}
+                toObject( database[type][z].tags.split(',').forEach(function(y){t[y]=y}) )
             }
         }
+
+        //then do complicated merging of tag objects
+        for (var z in database[type]) {
+            var a = database[type][z]
+            var proto = pickOne(database[type], {name: a.proto, type:type})
+
+            //merge tags from proto, unless there is a dash, which means don't merge
+            if (a.tagsObject && proto && proto.tagsObject && !a.tags.findChar('-')) {
+                //////////////////////////////////////////////////////////
+                //bob = {b:1}; sally = {s:2}; jack = {j:3}; sally = Object.setPrototypeOf(sally, bob); jack = Object.setPrototypeOf(jack, bob)
+                //narf = Object.setPrototypeOf(narf,jack); narf = _.extend(narf,sally);
+                //////////////////////////////////////////////////////////
+
+                //basically you gotta do the same thing for tags that you do for the word as a whole
+                if (a.tagsObject !== proto.tagsObject) {
+                    //add tags from prototype
+                    a.tagsObject = Object.setPrototypeOf(a.tagsObject, proto.tagsObject )
+                }
+                if ( a.parallelSense ) {
+                    //add any extra tags from parallel sense
+                    a.tagsObject = _.extend( a.tagsObject,  _.clone(a.parallelSense.tagsObject) )
+                                                            //clone grabs just the ownProperties
+                }
+            }
+        }
+
+        //convert tagsObject back to plain string tag property
+        for (var z in database[type]) {
+            if ("tagsObject".in(database[type][z])){
+                database[type][z].tags = _.keys( $.extend( {}, database[type][z].tagsObject ) ).join(',')
+                delete database[type][z].tagsObject
+                delete database[type][z].parallelSense
+            }
+        }
+        //end of tag merging
+
+
+    } //end of noun only section
+
+    //very final processing
+    for (var b in database[type]) {
+        // collapse prototype inheritance
+        database[type][b] = $.extend({},database[type][b])
+
+        // remove empty strings that were created from " " which were used to block prototype inheritance
+        prune(database[type][b])
     }
 
-    //convert tagsObject back to plain string tag property
-    for (var z in database[type]) {
-        if ("tagsObject".in(database[type][z])){
-            database[type][z].tags = _.keys( $.extend( {}, database[type][z].tagsObject ) ).join(',')
-            delete database[type][z].tagsObject
-            delete database[type][z].parallelSense
-        }
-    }
+
 }
 
 function setProto(a,type,x){
     if (a.proto) {
-        var proto = pickOne(database[type], {name: a.proto})
-        a = database[type][x] = Object.setPrototypeOf(a, proto )
+        var proto = pickOne(database[type], {name: a.proto, type:type})
+        if (!proto) error('Prototype "' + a.proto + '" could not be found for '+a.name+'.')
+        else a = database[type][x] = Object.setPrototypeOf(a, proto )
 
         if (Object.getPrototypeOf(a) === Object.prototype) //didn't take
-           { error('Prototype"' + a.proto + '"could not be found for'+a.name+'.')}
+           { error('Prototype assignment failed for '+a.name+'.')}
     }
     return a
 }
 
 //takes a word in the lexicon and creates all the same senses as its prototype (the word should have a prototype)
 function createSenses(word,type,number) {
-    number = number || word.name.replace(/.*?(\d*)$/g,'$1') || 1
-    var sense = word.proto+number
-    var proto_sense = pickOne(database[type], {name: sense})
+    if (!word.proto) return
 
-    if(proto_sense ) {
+    number = number || word.name.replace(/[^.]+\.?/,'') || 1
+    var sense = word.proto + ( number==1 ? "." : "") + number
+    var proto_sense = pickOne(database[type], {name: sense, type:type})
+
+    if(proto_sense) {
         if (proto_sense.name==word.name) return //don't create a sense when the word already is the sense
-        if (pickOne(database[type], {name: word.name.replace(/(.*?)\d*$/,'$1')+number})) return //don't create a sense if it is already defined
+        if (pickOne(database[type], {name: word.name.replace(/(.+?\.)\d*$/,'$1')+number, type:type})) return //don't create a sense if it is already defined
 
         var new_sense = {}
 
@@ -142,13 +193,14 @@ function createSenses(word,type,number) {
 
         //overwrite this senses values with the overwrite-values from the prototype sense
         //new_sense = _.extend($.extend({},proto_sense),$.extend({},new_sense)) //must be lo-dash extend because it only copies objects real properties!
-        new_sense = _.extend($.extend({},proto_sense),new_sense) //must be lo-dash extend because it only copies objects real properties!
+        //new_sense = _.extend(_.extend({},proto_sense),new_sense) //must be lo-dash extend because it only copies objects real properties!
+        new_sense = _.extend(new_sense,proto_sense) //must be lo-dash extend because it only copies objects real properties!
 
-        new_sense.name = word.name+number
+        new_sense.name = word.name + ( number==1 ? "." : "") + number
         new_sense.parallelSense = proto_sense
 
         //throw it in the database
-        database[type].push(new_sense)
+        lookup[type][new_sense.name] = database[type].push(new_sense) - 1
 
         //check for more senses
         createSenses(word,type,number+1)
@@ -156,6 +208,28 @@ function createSenses(word,type,number) {
     return new_sense
 }
 
+function addImpliedTags(tags){
+    var tagz = _.clone(tags)
+    var ont = _.cloneDeep(ontology)
+    if(!tags) return
+    tags.forEach(loop)
+
+    function loop(tag){
+        //if (!_.contains(ALL_TAGS,tag)) ALL_TAGS.push(tag)
+
+        if(typeof ont[tag]=='undefined') return []
+
+        ont[tag].forEach(function(t){
+            if (!_.contains(tags,t)) {
+                tags.push(t)
+                loop(t)
+            }
+        })
+
+    }
+
+    return tags
+}
 
 var database = { verb: [],noun: [], adjective: [],
 
@@ -182,22 +256,25 @@ var database = { verb: [],noun: [], adjective: [],
                     {name: 'all of the'},
                     {name: 'each of the', count: 1},
                     {name: 'a few of the', count: 1},
-                    {name: 'several', count: '1'},
-                    {name: 'numerous', count: '1'},
-                    {name: 'a few', count: '1'},
-                    {name: 'a couple of', count: '1'},
-                    {name: 'dozens of', count: '1'},
-                    {name: 'hundreds of', count: '1'},
-                    {name: 'many', count: '1'},
-                    {name: 'a number of', count: '1'},
-                    {name: 'much', count: '0'},
-                    {name: 'too much', count: '0'},
-                    {name: 'a little', count: '0'},
-                    {name: 'too little', count: '0'},
-                    {name: 'a bit of', count: '0'}
+                    {name: 'several', count: 1},
+                    {name: 'numerous', count: 1},
+                    {name: 'a few', count: 1},
+                    {name: 'a couple of', count: 1},
+                    {name: 'dozens of', count: 1},
+                    {name: 'hundreds of', count: 1},
+                    {name: 'many', count: 1},
+                    {name: 'a number of', count: 1},
+                    {name: 'much', count: 0},
+                    {name: 'too much', count: 0},
+                    {name: 'a little', count: 0},
+                    {name: 'too little', count: 0},
+                    {name: 'a bit of', count: 0}
                 ]
 
 }
+
+//index for fetching words by name, populated by processLexicon
+var lookup = {noun:{},verb:{},adjective:{}}
 
 var paradigms = {
 		verb: {tense: ['past','pres','fut'], number: ['sg','pl'], person: [1,2,3], aspect: ['simp', 'prog', 'retro', 'retroprog']},
@@ -236,7 +313,7 @@ var probabilities = {
     anim: [1,0, 1,1, 4,2, 5,3],
     tang: [1,0, 1,1, 2,2],
 
-    pronominal: [1,true,5,false], //how likely a NP is to be a pronoun instead
+    pronominal: [1,true,6,false], //how likely a NP is to be a pronoun instead
 
     //nounish
     number: [1,'pl', 3,'sg'],
